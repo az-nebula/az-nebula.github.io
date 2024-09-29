f6c430c38f0022e90387c1da832153a14130bc02
az <azure.nebula.z@gmail.com>
2024-09-29 23:27:05 +0900

    Add files via upload

---
+++ az_portal/js/works_list.js
@@ -0,0 +1,682 @@
+let works;
+works = {
+    t:'【違う冬のぼくら】ことのは、わかつ。 #06(end)',
+    c:'琴葉茜,琴葉葵,違う冬のぼくら',
+    y:'https://youtu.be/PNnUtM0ZraU',
+    n:'https://www.nicovideo.jp/watch/sm44059853',
+};
+nest(works);
+
+works = {
+    t:'【違う冬のぼくら】ことのは、わかつ。 #05',
+    c:'琴葉茜,琴葉葵,違う冬のぼくら',
+    y:'https://youtu.be/YO0umA3ddQE',
+    n:'https://www.nicovideo.jp/watch/sm44007314',
+};
+nest(works);
+
+works = {
+    t:'【違う冬のぼくら】ことのは、わかつ。 #04',
+    c:'琴葉茜,琴葉葵,違う冬のぼくら',
+    y:'https://youtu.be/U9pxk3EDsVk',
+    n:'https://www.nicovideo.jp/watch/sm43992468',
+};
+nest(works);
+
+works = {
+    t:'【違う冬のぼくら】ことのは、わかつ。 #03',
+    c:'琴葉茜,琴葉葵,違う冬のぼくら',
+    y:'https://youtu.be/5e-ykBvKEF4',
+    n:'https://www.nicovideo.jp/watch/sm43919097',
+};
+nest(works);
+
+works = {
+    t:'【違う冬のぼくら】ことのは、わかつ。 #02',
+    c:'琴葉茜,琴葉葵,違う冬のぼくら',
+    y:'https://youtu.be/fCyVBI2shcY',
+    n:'https://www.nicovideo.jp/watch/sm43918973',
+};
+nest(works);
+
+works = {
+    t:'すずきつづみ - 煽動的Sugary†Spice',
+    c:'すずきつづみ',
+    y:'https://youtu.be/itRWBhA5Okk',
+    n:'https://www.nicovideo.jp/watch/sm43959737',
+};
+nest(works);
+
+works = {
+    t:'【違う冬のぼくら】ことのは、わかつ。 #01',
+    c:'琴葉茜,琴葉葵,違う冬のぼくら',
+    y:'https://youtu.be/D1xbfKlkiww',
+    n:'https://www.nicovideo.jp/watch/sm43918662',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#18(end)',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/WSU-ym0mo1k',
+    n:'https://www.nicovideo.jp/watch/sm43911673',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#17',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/4lRS2WsH6xM',
+    n:'https://www.nicovideo.jp/watch/sm43911661',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#16',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/GAZNo0FESrY',
+    n:'https://www.nicovideo.jp/watch/sm43911638',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#15',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/XMvXdOfGqWs',
+    n:'https://www.nicovideo.jp/watch/sm43895024',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#14',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/NJClELkFU9g',
+    n:'https://www.nicovideo.jp/watch/sm43894825',
+};
+nest(works);
+
+works = {
+    t:'さとうささら - 激動的Spicy*Sugar',
+    c:'さとうささら',
+    y:'https://youtu.be/H9PKKrvcSX4',
+    n:'https://www.nicovideo.jp/watch/sm43896877',
+};
+nest(works);
+
+works = {
+    t:'【Library Of Ruina】接待はパワーなんです。',
+    c:'さとうささら,すずきつづみ,Library Of Ruina',
+    y:'https://youtu.be/4R0gqFmlqKs',
+    n:'',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#13',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/GW_5xsC-DxE',
+    n:'https://www.nicovideo.jp/watch/sm43869714',
+};
+nest(works);
+
+works = {
+    t:'すずきつづみ - 深杜',
+    c:'すずきつづみ',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm43863055',
+};
+nest(works);
+
+works = {
+    t:'すずきつづみ - 秘色',
+    c:'すずきつづみ',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm43831605',
+};
+nest(works);
+
+works = {
+    t:'【Pile Up! Box by Box】小箱りっか。#4(end)',
+    c:'小春六花,彩澄しゅお,Pile Up! Box by Box',
+    y:'https://youtu.be/trnJRhSUN_A',
+    n:'https://www.nicovideo.jp/watch/sm43860673',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#12',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/h3RS8w2GVGU',
+    n:'https://www.nicovideo.jp/watch/sm43852013',
+};
+nest(works);
+
+works = {
+    t:'【Library Of Ruina】指定司書つづみの備忘録。 - 泣く子も黙る反撃の狼煙',
+    c:'すずきつづみ,Library Of Ruina',
+    y:'https://youtu.be/uaR8zeqKlt8',
+    n:'https://www.nicovideo.jp/watch/sm43827186',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#11',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/BPSpjWzJIO4',
+    n:'https://www.nicovideo.jp/watch/sm43819492',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#10',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/O_NlXhSu9jw',
+    n:'https://www.nicovideo.jp/watch/sm43806218',
+};
+nest(works);
+
+works = {
+    t:'さとうささら - 杳都',
+    c:'さとうささら',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm43801248',
+};
+nest(works);
+
+works = {
+    t:'さとうささら - 汞巣',
+    c:'さとうささら',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm43798195',
+};
+nest(works);
+
+works = {
+    t:'さとうささら - 聴色',
+    c:'さとうささら',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm43798177',
+};
+nest(works);
+
+works = {
+    t:'【Pile Up! Box by Box】小箱りっか。#3',
+    c:'小春六花,彩澄しゅお,Pile Up! Box by Box',
+    y:'https://youtu.be/XiORhz6AeWM',
+    n:'https://www.nicovideo.jp/watch/sm43789667',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#09',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/YoglwLsa0co',
+    n:'https://www.nicovideo.jp/watch/sm43783341',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#08',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/njU6qON_FuM',
+    n:'https://www.nicovideo.jp/watch/sm43674117',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#07',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/Nsn9UtGk7wE',
+    n:'https://www.nicovideo.jp/watch/sm43666756',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#06',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/8R1PNHATX7I',
+    n:'https://www.nicovideo.jp/watch/sm43658713',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#05',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/ZY3IpLoUyCs',
+    n:'https://www.nicovideo.jp/watch/sm43641452',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#04',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/pjb5zPNWKh4',
+    n:'https://www.nicovideo.jp/watch/sm43616673',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#03',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/oTOHaPuhgUU',
+    n:'https://www.nicovideo.jp/watch/sm43607723',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#02',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/62H7Sq0dDDc',
+    n:'https://www.nicovideo.jp/watch/sm43607714',
+};
+nest(works);
+
+works = {
+    t:'【進め! キノピオ隊長】探検隊員みゃーまいの冒険。#01',
+    c:'宮舞モカ,進め! キノピオ隊長',
+    y:'https://youtu.be/ymzh2uo_lYk',
+    n:'https://www.nicovideo.jp/watch/sm43607703',
+};
+nest(works);
+
+works = {
+    t:'【Pile Up! Box by Box】小箱りっか。#2',
+    c:'小春六花,彩澄しゅお,Pile Up! Box by Box',
+    y:'https://youtu.be/farN7EvrVN4',
+    n:'https://www.nicovideo.jp/watch/sm43591905',
+};
+nest(works);
+
+works = {
+    t:'【Pile Up! Box by Box】小箱りっか。#1',
+    c:'小春六花,彩澄しゅお,Pile Up! Box by Box',
+    y:'https://youtu.be/WN_hJKRnPtU',
+    n:'https://www.nicovideo.jp/watch/sm43448893',
+};
+nest(works);
+
+works = {
+    t:'さとうささら - トコシエのワルツ【Cover】',
+    c:'さとうささら',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm43483790',
+};
+nest(works);
+
+works = {
+    t:'【FILMECHANISM】ふぃるあかりずむ。#6(end)',
+    c:'紲星あかり,FILMECHANISM',
+    y:'https://youtu.be/RyoVnr8L1UY',
+    n:'https://www.nicovideo.jp/watch/sm43422629',
+};
+nest(works);
+
+works = {
+    t:'【FILMECHANISM】ふぃるあかりずむ。#5',
+    c:'紲星あかり,FILMECHANISM',
+    y:'https://youtu.be/KHn6RM71m6I',
+    n:'https://www.nicovideo.jp/watch/sm43409543',
+};
+nest(works);
+
+works = {
+    t:'すずきつづみ - AM2:23の仲直り',
+    c:'すずきつづみ',
+    y:'https://youtu.be/BmSBtQNjhLY',
+    n:'https://www.nicovideo.jp/watch/sm43382263',
+};
+nest(works);
+
+works = {
+    t:'【FILMECHANISM】ふぃるあかりずむ。#4',
+    c:'紲星あかり,FILMECHANISM',
+    y:'https://youtu.be/r1Ma4mX8b0I',
+    n:'https://www.nicovideo.jp/watch/sm43346643',
+};
+nest(works);
+
+works = {
+    t:'【FILMECHANISM】ふぃるあかりずむ。#3',
+    c:'紲星あかり,FILMECHANISM',
+    y:'https://youtu.be/WDdgSMc4ews',
+    n:'https://www.nicovideo.jp/watch/sm43335000',
+};
+nest(works);
+
+works = {
+    t:'【FILMECHANISM】ふぃるあかりずむ。#2',
+    c:'紲星あかり,FILMECHANISM',
+    y:'https://youtu.be/-UWtEQGyEPI',
+    n:'https://www.nicovideo.jp/watch/sm43322345',
+};
+nest(works);
+
+works = {
+    t:'【FILMECHANISM】ふぃるあかりずむ。#1',
+    c:'紲星あかり,FILMECHANISM',
+    y:'https://youtu.be/pRNDuvSvEiI',
+    n:'https://www.nicovideo.jp/watch/sm43318566',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その13(end)',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/4yOeQDRKiiQ',
+    n:'https://www.nicovideo.jp/watch/sm43231840',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その12',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/UXhDtyH5WkA',
+    n:'https://www.nicovideo.jp/watch/sm43220473',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その11',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/Z8Qw1vAqJ6w',
+    n:'https://www.nicovideo.jp/watch/sm43208213',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その10',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/RHiC7PRE5x0',
+    n:'https://www.nicovideo.jp/watch/sm43132645',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その9',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/wA2EZYTGSmY',
+    n:'https://www.nicovideo.jp/watch/sm43096255',
+};
+nest(works);
+
+works = {
+    t:'すずきつづみ&さとうささら&タカハシアマト - 幾星霜の「　」',
+    c:'さとうささら,すずきつづみ,タカハシアマト',
+    y:'https://youtu.be/F4j9oCSsUXc',
+    n:'https://www.nicovideo.jp/watch/sm43090004',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その8',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/52Gv662mT9k',
+    n:'https://www.nicovideo.jp/watch/sm43025441',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その7',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/qRyIwjzNkYs',
+    n:'https://www.nicovideo.jp/watch/sm42887612',
+};
+nest(works);
+
+works = {
+    t:'【ソフィーのアトリエ2】ささらちゃんが絶海の覇王(笑)を蹂躙するだけ',
+    c:'さとうささら,ソフィーのアトリエ2',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm42839836',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その6',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/VRKU744LF28',
+    n:'https://www.nicovideo.jp/watch/sm42811302',
+};
+nest(works);
+
+works = {
+    t:'物語を紡ぐ舞台',
+    c:'さとうささら,すずきつづみ,タカハシアマト',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm42797622',
+};
+nest(works);
+
+works = {
+    t:'さとうささら∞すずきつづみ - PRESEA',
+    c:'さとうささら,すずきつづみ',
+    y:'https://youtu.be/ZtRIF947Cw4',
+    n:'https://www.nicovideo.jp/watch/sm42785406',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その5',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/w5ypnQIyV6s',
+    n:'https://www.nicovideo.jp/watch/sm42770821',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その4',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/ZIHL_-B3wKU',
+    n:'https://www.nicovideo.jp/watch/sm42739298',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その3',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/wFayAC99Y7w',
+    n:'https://www.nicovideo.jp/watch/sm42703149',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その2',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/wHbGVRwXTe4',
+    n:'https://www.nicovideo.jp/watch/sm42676056',
+};
+nest(works);
+
+works = {
+    t:'さとうささら&すずきつづみ - SOMNIUM',
+    c:'さとうささら,すずきつづみ',
+    y:'https://youtu.be/mllOV5IE61I',
+    n:'https://www.nicovideo.jp/watch/sm42668421',
+};
+nest(works);
+
+works = {
+    t:'【QV】ささら、じげんをなおす。その1',
+    c:'さとうささら,すずきつづみ,QV',
+    y:'https://youtu.be/rotuOXcKk0I',
+    n:'https://www.nicovideo.jp/watch/sm42646535',
+};
+nest(works);
+
+works = {
+    t:'stellar*checkmate - モラトリアム崩壊症候群',
+    c:'さとうささら,すずきつづみ,双葉湊音,小春六花,夏色花梨,花隈千冬',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm426290606',
+};
+nest(works);
+
+works = {
+    t:'小春六花の歌声なら、架空言語でも違和ない説',
+    c:'小春六花',
+    y:'https://youtu.be/rZgVYS7J8rw',
+    n:'https://www.nicovideo.jp/watch/sm42585591',
+};
+nest(works);
+
+works = {
+    t:'アナグラム',
+    c:'さとうささら,すずきつづみ',
+    y:'https://youtu.be/_G-9iQNr8GY',
+    n:'https://www.nicovideo.jp/watch/sm42570676',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃12(end)',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/dCzCHT3WJUg',
+    n:'',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃11',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/2Hci17dpp5M',
+    n:'https://www.nicovideo.jp/watch/sm42540696',
+};
+nest(works);
+
+works = {
+    t:'おこりっか',
+    c:'小春六花',
+    y:'https://youtu.be/LSeX5KQgFY4',
+    n:'https://www.nicovideo.jp/watch/sm42536480',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃10',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/oZCmST7Wuxs',
+    n:'https://www.nicovideo.jp/watch/sm42511917',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃9',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/vw9Diej7NDA',
+    n:'https://www.nicovideo.jp/watch/sm42489304',
+};
+nest(works);
+
+works = {
+    t:'さとうささら - 晃',
+    c:'さとうささら',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm42489236',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃8',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/2h-WdsJSqoM',
+    n:'https://www.nicovideo.jp/watch/sm42482112',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃7',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/bA1WgUqg5co',
+    n:'https://www.nicovideo.jp/watch/sm42465241',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃6',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/ZxmuZzJIb-w',
+    n:'https://www.nicovideo.jp/watch/sm42396659',
+};
+nest(works);
+
+works = {
+    t:'ブチギレっか',
+    c:'小春六花',
+    y:'https://youtu.be/-Tf-FBcD7Rk',
+    n:'https://www.nicovideo.jp/watch/sm42376592',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃5',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/FWFN15ahKlg',
+    n:'https://www.nicovideo.jp/watch/sm42337670',
+};
+nest(works);
+
+works = {
+    t:'すずきつづみ - 灑涙雨',
+    c:'すずきつづみ',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm42318393',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃4',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/JYGTj7V19dA',
+    n:'https://www.nicovideo.jp/watch/sm42303527',
+};
+nest(works);
+
+works = {
+    t:'さとうささら - ささらちゃんがSRSR歌うだけ',
+    c:'さとうささら',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm42277755',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃3',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/fv4c1h5741Q',
+    n:'https://www.nicovideo.jp/watch/sm42267930',
+};
+nest(works);
+
+works = {
+    t:'六花花梨千冬ささらつづみ - Clover',
+    c:'小春六花,夏色花梨,花隈千冬,さとうささら,すずきつづみ',
+    y:'',
+    n:'https://www.nicovideo.jp/watch/sm422354520',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃2',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/I3_D3OfX5_Y',
+    n:'https://www.nicovideo.jp/watch/sm42195146',
+};
+nest(works);
+
+works = {
+    t:'【さかだちの街】りっか、さかだちする。＃1',
+    c:'小春六花,紲星あかり,さかだちの街',
+    y:'https://youtu.be/vPL_IuiOGgQ',
+    n:'https://www.nicovideo.jp/watch/sm42182989',
+};
+nest(works);
+
+//仮リストの生成
+function nest(works) {
+    const works_log = document.getElementById("works-log");
+    works_log.innerHTML = works_log.innerHTML +
+        '<ul><li>' + works.t + '</li>\
+        <li>' + works.c + '</li>\
+        <li>' + works.y + '</li>\
+        <li>' + works.n + '</li></ul>';
+};
